dfa2157b726e2ea0fbae26dd23aca2449a538a73
Abduaziz Kayumov <aziz@azizs-mbp14.local>
2023-09-28 21:43:21 +0900

    closes #5

--- src/cut.rs
+++ src/cut.rs
@@ -15,7 +15,9 @@ pub fn cut(forest: &mut Vec<Node>, v: usize) {
 mod tests {
     use crate::{
         connected::connected,
-        node::{Node, Parent}, link::link,
+        cut::cut,
+        link::link,
+        node::{Node, Parent},
     };
 
     fn create_nodes(n: usize) -> Vec<Node> {
@@ -26,7 +28,7 @@ mod tests {
     pub fn base_case() {
         let mut forest = create_nodes(2);
         assert!(!connected(&mut forest, 0, 1)); // not connected yet
-        
+
         link(&mut forest, 0, 1);
         assert!(matches!(forest[1].parent, Parent::Root));
         assert_eq!(forest[1].right, Some(0));
@@ -44,13 +46,49 @@ mod tests {
         //    0             1
         //   /      =>      |
         //  1               0
-        
+
         super::cut(&mut forest, 0);
         assert!(matches!(forest[0].parent, Parent::Root));
         assert_eq!(forest[0].right, None);
         //    0           0
-        //   /     =>     
+        //   /     =>
         //  1           1
         assert!(!connected(&mut forest, 0, 1)); // now disconnected
+    }
+
+    #[test]
+    pub fn cut_into_two_subtrees() {
+        let mut forest = create_nodes(5);
+        forest[0].left = Some(1);
+        forest[1].parent = Parent::Node(0);
+        forest[1].left = Some(2);
+        forest[2].parent = Parent::Node(1);
+        forest[3].right = Some(4);
+        forest[4].parent = Parent::Node(3);
+        // Given two trees:
+        //       0       3
+        //      /         \
+        //     1           4
+        //    /
+        //   2
+        link(&mut forest, 2, 3);
+        // link(2, 3) should result in:
+        //      3
+        //      | \
+        //      4  2
+        //         |
+        //         1
+        //          \
+        //           0
+        assert!(matches!(forest[3].parent, Parent::Root));
+        assert!(matches!(forest[4].parent, Parent::Path(3)));
+        assert_eq!(forest[3].right, Some(2));
+        assert!(matches!(forest[2].parent, Parent::Node(3)));
+        assert!(matches!(forest[1].parent, Parent::Path(2)));
+        assert_eq!(forest[1].right, Some(0));
+        assert!(connected(&mut forest, 2, 3));
+        cut(&mut forest, 2);
+        assert!(!connected(&mut forest, 2, 3));
+        assert!(!connected(&mut forest, 2, 4));
     }
 }
