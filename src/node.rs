61627062599ee798b318834982c0494bfafcefbc
azizkayumov <kayumovabduaziz@gmail.com>
2023-11-11 21:44:23 +0900

    skip to_str()

--- src/node.rs
+++ src/node.rs
@@ -39,6 +39,7 @@ impl<T: Path> Node<T> {
     }
 
     #[allow(dead_code)]
+    #[cfg_attr(tarpaulin, ignore)]
     pub fn to_str(&self) -> String {
         let parent = match self.parent {
             Parent::Node(idx) => format!("Node({idx})"),
