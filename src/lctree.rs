80944b8fbe9200d244ccdaa92c60afaca2baab33
azizkayumov <kayumovabduaziz@gmail.com>
2023-11-12 04:47:04 +0900

    fix tests

--- src/lctree.rs
+++ src/lctree.rs
@@ -355,60 +355,27 @@ mod tests {
     }
 
     #[test]
-    pub fn invalid_link() {
+    pub fn connected_so_no_need_to_link() {
         let mut lctree = super::LinkCutTree::default();
         let alice = lctree.make_tree(0.0);
         let bob = lctree.make_tree(10.0);
         let clay = lctree.make_tree(2.0);
         lctree.link(alice, bob);
         lctree.link(bob, clay);
-        lctree.link(alice, clay); // should do nothing
-
-        // bob should have the max weight in the path from alice to clay:
-        let heaviest_node = lctree.path(alice, clay);
-        assert_eq!(heaviest_node.idx, bob);
-        assert_eq!(heaviest_node.weight, 10.0);
+        // Try to link two nodes that are already connected:
+        assert!(!lctree.link(alice, clay));
     }
 
     #[test]
-    pub fn invalid_cut() {
+    pub fn connected_but_no_edge_to_cut() {
         let mut lctree = super::LinkCutTree::default();
         let alice = lctree.make_tree(0.0);
         let bob = lctree.make_tree(10.0);
         let clay = lctree.make_tree(2.0);
         lctree.link(alice, bob);
-
-        let path = lctree.path(alice, clay);
-        lctree.cut(alice, clay); // should do nothing
-        let same_path = lctree.path(alice, clay);
-        // path from alice to clay should be the same as before:
-        assert_eq!(path.idx, same_path.idx);
-        assert_eq!(path.weight, same_path.weight);
-    }
-
-    #[test]
-    pub fn connected_but_no_edge_to_cut() {
-        // We form a link-cut tree from the following rooted tree:
-        //     a
-        //    / \
-        //   b   c
-        //        \
-        //         d
-        let mut lctree = super::LinkCutTree::default();
-        let a = lctree.make_tree(0.0);
-        let b = lctree.make_tree(0.0);
-        let c = lctree.make_tree(0.0);
-        let d = lctree.make_tree(0.0);
-
-        lctree.link(b, a);
-        lctree.link(c, a);
-        lctree.link(d, c);
-
-        // Try to cut non-existing edge:
-        lctree.cut(d, a); // should do nothing
-
-        // They should still be connected:
-        assert!(lctree.connected(d, a));
+        lctree.link(bob, clay);
+        // Try to cut an edge that doesn't exist:
+        assert!(!lctree.cut(alice, clay));
     }
 
     #[test]
@@ -423,6 +390,7 @@ mod tests {
 
         assert!(lctree.linked(alice, bob));
         assert!(lctree.linked(bob, clay));
+        // alice and clay are not connected by a link
         assert!(!lctree.linked(alice, clay));
     }
 
