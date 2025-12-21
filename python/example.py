ad800a90f5e3052d5663d8467696e49d0efddd6c
Aziz Kayumov <kayumovabduaziz@gmail.com>
2025-12-21 23:52:10 +0500

    Add example pyo3 usage

---
+++ python/example.py
@@ -0,0 +1,4 @@
+from python import sum_as_string
+
+a = sum_as_string(3, 5)
+print(f"The sum is: {a}")
--- src/node.rs
+++ src/node.rs
@@ -39,7 +39,6 @@ impl<T: Path> Node<T> {
     }
 
     #[allow(dead_code)]
-    #[cfg(not(tarpaulin_include))]
     pub fn to_str(&self) -> String {
         let parent = match self.parent {
             Parent::Node(idx) => format!("Node({idx})"),
