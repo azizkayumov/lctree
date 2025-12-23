eccefbec986fdb6d31e6568cc6fca4b17272fe33
Aziz Kayumov <kayumovabduaziz@gmail.com>
2025-12-23 18:23:36 +0500

    Add wrapper struct

--- python/example.py
+++ python/example.py
@@ -1,4 +1,28 @@
-from python import sum_as_string
+from _lctree import LinkCutTree
 
-a = sum_as_string(3, 5)
-print(f"The sum is: {a}")
+tree = LinkCutTree()
+
+a = tree.make_tree(9.0)
+b = tree.make_tree(1.0)
+c = tree.make_tree(8.0)
+d = tree.make_tree(10.0)
+e = tree.make_tree(2.0)
+f = tree.make_tree(4.0)
+
+tree.link(b, a)
+tree.link(c, b)
+tree.link(d, b)
+tree.link(e, a)
+tree.link(f, e)
+
+assert tree.connected(c, f) == True # connected
+
+(max_idx, max_weight) = tree.find_max(c, f)
+assert max_idx == a
+assert max_weight == 9.0
+
+tree.cut(e, a)
+
+assert tree.connected(c, f) == False # not connected anymore
+
+print("All tests passed!")
--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,13 +1,45 @@
+use lctree::FindMax;
+use lctree::LinkCutTree;
 use pyo3::prelude::*;
 
-/// A Python module implemented in Rust.
-#[pymodule]
-mod python {
-    use pyo3::prelude::*;
+#[pyclass(name = "LinkCutTree")]
+pub struct LinkCutTreeWrapper {
+    lctree: LinkCutTree<FindMax>,
+}
 
-    /// Formats the sum of two numbers as string.
-    #[pyfunction]
-    fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-        Ok((a + b).to_string())
+#[pymethods]
+impl LinkCutTreeWrapper {
+    #[new]
+    fn new() -> Self {
+        let mut lctree = LinkCutTree::default();
+        lctree.make_tree(0.0);
+        LinkCutTreeWrapper { lctree }
     }
+
+    fn make_tree(&mut self, value: f64) -> usize {
+        self.lctree.make_tree(value)
+    }
+
+    fn link(&mut self, u: usize, v: usize) -> bool {
+        self.lctree.link(u, v)
+    }
+
+    fn cut(&mut self, u: usize, v: usize) -> bool {
+        self.lctree.cut(u, v)
+    }
+
+    fn connected(&mut self, u: usize, v: usize) -> bool {
+        self.lctree.connected(u, v)
+    }
+
+    fn find_max(&mut self, u: usize, v: usize) -> (usize, f64) {
+        let max = self.lctree.path(u, v);
+        (max.idx, max.weight)
+    }
+}
+
+#[pymodule]
+fn _lctree(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<LinkCutTreeWrapper>()?;
+    Ok(())
 }
